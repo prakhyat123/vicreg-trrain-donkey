120375c7e4e68428d38500ec6a49ed67b425ba77
prakhyat123 <53633577+prakhyat123@users.noreply.github.com>
2022-12-31 22:57:20 +0530

    Update wrapper.py

--- vicregdonkey/wrapper.py
+++ vicregdonkey/wrapper.py
@@ -9,12 +9,12 @@ import numpy as np
 class VICREGWrapper(gym.Wrapper):
     def __init__(self, env: gym.Env):
         super().__init__(env)
-        self.resnet = torch.hub.load('pytorch/vision:v0.10.0','resnet18',pretrained=True)
+        self.resnet = torch.hub.load('pytorch/vision:v0.10.0','resnet18',pretrained=False)
         DATA_PATH = pkg_resources.resource_filename('vicregdonkey', '/')
         model_name="resnet18.pth"
         self.resnet.load_state_dict(torch.load(DATA_PATH+model_name))
         self.resnet.eval()
-        self.resnet.cuda()
+        #self.resnet.cuda()
         self.data_transforms = TrainTransform()
         self.observation_space = gym.spaces.Box(
             low = -np.inf,
@@ -28,12 +28,12 @@ class VICREGWrapper(gym.Wrapper):
         # Encode with the pre-trained VICREG
         obs = self.data_transforms(obs).unsqueeze(0)
         with torch.no_grad():       
-            encoded_image = self.resnet(obs.cuda()).cpu()
+            encoded_image = self.resnet(obs).cpu()
         return encoded_image, reward, done, infos
 
     def reset(self) -> np.ndarray:
         obs = self.env.reset()
         obs = self.data_transforms(obs).unsqueeze(0)
         with torch.no_grad():
-            encoded_image = self.resnet(obs.cuda()).cpu()
+            encoded_image = self.resnet(obs).cpu()
         return encoded_image
